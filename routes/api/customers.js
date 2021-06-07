018b22e606dec98817d3f9ba858d623e6c5625dc
vawn <vantcde130099@fpt.edu.vn>
2021-06-07 20:15:38 +0700

    fix login with facebook

--- routes/api/customers.js
+++ routes/api/customers.js
@@ -4,6 +4,129 @@ const router = express.Router();
 // @route   GET api/users
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => res.send('Customers route'));
+router.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+// @route   GET api/customers/auth/google/callback
+// @desc    get response from google api & return token for FE
+// @access  Public
+router.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    //return jsonwebtoken
+    const payload = {
+      customer: {
+        id: req.user.id
+      }
+    };
+
+    jwt.sign(
+      //sign the token pass and the payload pass
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 36000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token }); //if have no err, send that token to the client
+      }
+    );
+
+    // Successful authentication, redirect home.
+    // res.redirect('/');
+  }
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: facebookID,
+      clientSecret: facebookSecret,
+      callbackURL: '/api/customers/auth/facebook/callback',
+      profileFields: ['emails', 'displayName', 'photos']
+    },
+    async function(accessToken, refreshToken, profile, done) {
+      await Customer.findOne({ facebookId: profile.id }).then(currentUser => {
+        if (currentUser) {
+          done(null, currentUser);
+        } else {
+          new Customer({
+            facebookId: profile.id,
+            name: profile.displayName,
+            // contact: { email: profile.emails[0].value },
+            avatar: profile.photos
+              ? profile.photos[0].value
+              : '/img/faces/unknown-user-pic.jpg'
+          })
+            .save()
+            .then(newCustomer => {
+              done(null, newCustomer);
+            });
+        }
+      });
+    }
+  )
+);
+
+// @route   GET api/customers/auth/facebook
+// @desc    get token user by facebook api
+// @access  Public
+router.get('/auth/facebook', passport.authenticate('facebook'));
+
+// @route   GET api/customers/auth/facebook/callback
+// @desc    get response from facebook api & return token for FE
+// @access  Public
+router.get(
+  '/auth/facebook/callback',
+  passport.authenticate('facebook', {
+    failureRedirect: '/login'
+  }),
+  (req, res) => {
+    //return jsonwebtoken
+    console.log(req);
+    const payload = {
+      customer: {
+        id: req.user.id
+      }
+    };
+
+    jwt.sign(
+      //sign the token pass and the payload pass
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 36000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token }); //if have no err, send that token to the client
+      }
+    );
+  }
+);
+// @route   GET api/customer
+// @desc    get all customer
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    let allCustomers = await Customers.find();
+    if (!allCustomers) {
+      return res.status(400).json({ message: 'No Customer found' });
+    }
+    res.json(allCustomers);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ message: 'Lỗi server' });
+  }
+});
+
+// @route   GET api/customers/current
+// @desc    get current customer
+// @access  Public
+router.get('/current', async (req, res) => {
+  res.send(req.customer);
+});
 
 module.exports = router;
