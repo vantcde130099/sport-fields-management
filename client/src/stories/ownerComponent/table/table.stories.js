3db5678ea9a615ed28daca4320eb562186dad5dd
Phạm Trung Nam <namptde130115@fpt.edu.vn>
2021-06-15 15:11:34 +0700

    storybook table owner component

---
+++ client/src/stories/ownerComponent/table/table.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { EnhancedTable } from './table';
+
+export default {
+    title: 'Mui/Table',
+    component: EnhancedTable
+}
+
+export const Table = () => <EnhancedTable></EnhancedTable>;
