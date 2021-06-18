c1bcd702e8925c38e6aee4cf762a04823292f867
minhnh <minhnhde130140@fpt.edu.vn>
2021-06-18 08:49:15 +0700

    update header .

--- client/src/components/NotificationList/ListItemNotificationUser.tsx
+++ client/src/components/NotificationList/ListItemNotificationUser.tsx
@@ -1,36 +1,16 @@
 import React from 'react'
-import List from '@material-ui/core/List';
 import {  makeStyles } from '@material-ui/core/styles';
 import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
 import Avatar from '@material-ui/core/Avatar';
-import ListItem from '@material-ui/core/ListItem';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import AccountBoxRoundedIcon from '@material-ui/icons/AccountBoxRounded';
-import NaturePeopleIcon from '@material-ui/icons/NaturePeople';
 export interface ListItemNotificationUserProps {
     isMenuOpen ?: boolean  
     image ?: any
   }
 
-const useStyles = makeStyles((theme) => ({
-    grow: {
-      flexGrow: 1,
-    },
-    root: {
-      width: '100%',
-      maxWidth: '36ch',
-      margin : 0 ,
-      padding : 0 ,
-      backgroundColor: theme.palette.background.paper,
-      fontWeight : 500,
-    },
-    inline: {
-        display: 'inline',
-      },
-}))
 export  function ListItemNotificationUser({image = "https://www.dungplus.com/wp-content/uploads/2019/12/girl-xinh-1-480x600.jpg" , isMenuOpen=true,...props}) {
-    const classes = useStyles();
     return (
         <Menu
         anchorEl={props.anchorEl}
