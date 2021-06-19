ec85d470911cad8a89c2073014ec83a55dd1c718
minhnh <minhnhde130140@fpt.edu.vn>
2021-06-19 10:52:42 +0700

    update

--- client/src/components/ListCard/ListCard.tsx
+++ client/src/components/ListCard/ListCard.tsx
@@ -1,42 +1,38 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import FormLabel from '@material-ui/core/FormLabel';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import Radio from '@material-ui/core/Radio';
-import Paper from '@material-ui/core/Paper';
-import CardField  from '../card/CardField'
-import { Container } from '@material-ui/core';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import FormLabel from '@material-ui/core/FormLabel'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import Radio from '@material-ui/core/Radio'
+import Paper from '@material-ui/core/Paper'
+import CardField from '../card/CardField'
+import { Container } from '@material-ui/core'
+export interface Props {
+  listItem?: []
+}
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-  }));
-export default function ListCard() {
-    const [spacing, setSpacing] = React.useState(2);
-    const classes = useStyles();
-  
-    const handleChange = (event : any) => {
-      setSpacing(Number(event.target.value));
-    };
-  
-    return (
-        <Container maxWidth="md">
+  root: {
+    flexGrow: 1
+  }
+}))
+export const ListCard: React.FC<Props> = ({ listItem = [] }) => {
+  const [spacing, setSpacing] = React.useState(2)
+  const classes = useStyles()
+
+  const handleChange = (event: any) => {
+    setSpacing(Number(event.target.value))
+  }
+
+  return (
+    <Container maxWidth="md">
       <Grid container className={classes.root} spacing={5}>
-        <Grid item xs={4}>
-            <CardField/>
-        </Grid> 
-        <Grid item xs={4}>
-            <CardField/>
-        </Grid> 
-        <Grid item xs={4}>
-            <CardField/>
-        </Grid>        
-        <Grid item xs={4}>
-            <CardField/>
-        </Grid> 
+        {listItem.map((item) => (
+          <Grid item xs={4}>
+            <CardField item={item} />
+          </Grid>
+        ))}
       </Grid>
-      </Container>
-      )
+    </Container>
+  )
 }
--- client/src/components/card/CardField.styles.tsx
+++ client/src/components/card/CardField.styles.tsx
@@ -1,88 +1,88 @@
-import { makeStyles ,ThemeProvider , createMuiTheme } from '@material-ui/core/styles';
+import {
+  makeStyles,
+  ThemeProvider,
+  createMuiTheme
+} from '@material-ui/core/styles'
 
 export const useStyles = makeStyles({
-    "@global": {
-      "@keyframes fadeIn": {
-        "0%": {
-          opacity: 0,
-        },
-        "100%": {
-          opacity: 1,
-        }
+  '@global': {
+    '@keyframes fadeIn': {
+      '0%': {
+        opacity: 0
+      },
+      '100%': {
+        opacity: 1
+      }
+    }
+  },
+  selector: {
+    animation: 'fadeIn .5s ease-in-out' // --> this works
+  },
+  root: {
+    maxWidth: 280,
+    borderRadius: 9
+  },
+  hoverShowAll: {},
+  customBox: {
+    display: '-webkit-box',
+    boxOrient: 'vertical',
+    lineClamp: 1,
+    wordBreak: 'break-all',
+    overflow: 'hidden'
+  },
+  media: {
+    '&:hover': {
+      '& $contentMedia': {
+        animation: '$fadeIn .6s ease-in-out',
+        display: 'block'
       }
     },
-    selector: {
-      animation: "fadeIn .5s ease-in-out" // --> this works
-    },
-    root: {
-      maxWidth: 280,
-      borderRadius : 9,
-    },
-    hoverShowAll:{
-  
-    },
-    customBox: {
-      display: "-webkit-box",
-      boxOrient: "vertical",
-      lineClamp: 1,
-      wordBreak: "break-all",
-      overflow: "hidden"
-    },
-    media: {
-      '&:hover':{
-        "& $contentMedia" : {
-          animation: "$fadeIn .6s ease-in-out",
-          display : "block",
-        },
-      },
-      position :"relative",
-      height: 170,
-    },
-    hoverDisabled :{
-    },
-    contentMedia :{
-      display : "none",
-      boxSizing : "border-box",
-      padding : 5,
-      position : "absolute",
-      bottom : 0 ,
-      left : 0,
-      width : '100%',
-      backgroundColor :'rgba(0, 0, 0, 0.3)',
-      color :"white", 
-    }
-    ,
-    content:{
-      height : 75,
-      fontWeight : 100,
-      paddingTop : 10 ,
-      paddingBottom : 10 ,
-    },
-    button: {
-      fontSize : 8,
-      color : 'white',
-      backgroundColor : "#F94949",
-      // fontSize : "12px"
-    },
-    starIcon:{
-      position : "absolute"
-      ,top : 0 
-      , right : 0
-    },
-    costsButton : {
-      color: '#F94949',
-      fontWeight : 400,
-      borderColor : '#F94949'
-    },
-    flexInline : {
-      display : "flex"
-    },
-    typographyContent :{
-      fontWeight : 300,
-      fontSize : 20
-    },
-    typographyDescription :{
-      position : "relative",
-      fontWeight: 300 ,
-    },
-  });
+    position: 'relative',
+    height: 170
+  },
+  hoverDisabled: {},
+  contentMedia: {
+    display: 'none',
+    boxSizing: 'border-box',
+    padding: 5,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    color: 'white'
+  },
+  content: {
+    height: 75,
+    fontWeight: 100,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  button: {
+    fontSize: 8,
+    color: 'white',
+    backgroundColor: '#F94949'
+    // fontSize : "12px"
+  },
+  starIcon: {
+    position: 'absolute',
+    top: 0,
+    right: 0
+  },
+  costsButton: {
+    color: '#F94949',
+    fontWeight: 400,
+    borderColor: '#F94949'
+  },
+  flexInline: {
+    display: 'flex'
+  },
+  typographyContent: {
+    fontWeight: 300,
+    fontSize: 20
+  },
+  typographyDescription: {
+    position: 'relative',
+    fontWeight: 300
+  }
+})
