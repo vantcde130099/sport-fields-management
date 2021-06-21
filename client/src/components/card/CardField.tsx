1a7f26c0033446f51ea6ae520c0aebb058e81d91
minhnh <minhnhde130140@fpt.edu.vn>
2021-06-21 09:58:57 +0700

    update

--- client/src/components/card/CardField.tsx
+++ client/src/components/card/CardField.tsx
@@ -1,14 +1,16 @@
 import React, { Component, useState } from 'react'
-import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
-import Paper from '@material-ui/core/Paper'
-import { useStyles } from './CardField.styles'
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  Paper
+} from '@material-ui/core'
 import Rating from '@material-ui/lab/Rating'
+import { useStyles } from './CardField.styles'
 
 export interface CardProps {
   item?: {
