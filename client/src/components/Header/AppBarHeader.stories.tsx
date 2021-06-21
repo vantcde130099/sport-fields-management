a8bbe42dbf93db76f355e3010bcf592cd56db085
minhnh <minhnhde130140@fpt.edu.vn>
2021-06-21 12:55:12 +0700

    update header

--- client/src/components/Header/AppBarHeader.stories.tsx
+++ client/src/components/Header/AppBarHeader.stories.tsx
@@ -12,7 +12,11 @@ const Template: Story<AppBarHeaderProps> = (args) => <AppBarHeader {...args} />;
 
 export const HeaderLogin = Template.bind({});
 HeaderLogin.args = {
-    user: {}
+    user: {
+      name : "Minh",
+      img : "https://www.dungplus.com/wp-content/uploads/2019/12/girl-xinh-1-480x600.jpg"
+      ,tel : "0923174331"
+    }
 };
 
 export const HeaderLogout = Template.bind({});
--- client/src/components/NotificationList/ListItemNotificationUser.stories.tsx
+++ client/src/components/NotificationList/ListItemNotificationUser.stories.tsx
@@ -14,6 +14,9 @@ const Template: Story<ListItemNotificationUserProps> = args => <ListItemNotifica
 export const ItemUser = Template.bind({});
 ItemUser.args = {
     isMenuOpen : true,
+    name : "Minh",
+    img : "https://www.dungplus.com/wp-content/uploads/2019/12/girl-xinh-1-480x600.jpg"
+    ,tel : "0923174331"
 };
 
 
