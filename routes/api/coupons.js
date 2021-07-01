6995e385028ae60ecb0cca2848db8b5c452de8d5
Vanw <vantcde130099@fpt.edu.vn>
2021-07-01 09:14:55 +0700

    fix tach code

--- routes/api/coupons.js
+++ routes/api/coupons.js
@@ -26,6 +26,7 @@ router.post(
   ],
   async (req, res) => {
     const errors = validationResult(req)
+
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() })
     }
