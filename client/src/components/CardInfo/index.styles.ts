54a51c19ff22f5ce34cca3f6a4879321be70338d
minhnh <minhnhde130140@fpt.edu.vn>
2021-07-01 22:21:59 +0700

    cac

---
+++ client/src/components/CardInfo/index.styles.ts
@@ -0,0 +1,32 @@
+import { createMuiTheme, makeStyles } from '@material-ui/core/styles'
+
+import { Props } from '.'
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#F94949'
+    }
+  }
+});
+
+export const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    width : 250,
+    minWidth: 120,
+  },
+  paper: {
+    padding: theme.spacing(2),
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  customBox: {
+    display: '-webkit-box',
+    boxOrient: 'vertical',
+    lineClamp: 2,
+    wordBreak: 'break-all',
+    overflow: 'hidden'
+  },
+}));
