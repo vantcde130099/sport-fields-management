e04fd02d510ff3fc32204aaf964327cbee51e992
Phạm Trung Nam <namptde130115@fpt.edu.vn>
2021-07-01 16:54:53 +0700

    fix code review

--- client/src/pages/login/loginUser.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { LoginUser } from './login'
-
-export default {
-  title: 'Mui/loginPage',
-  component: LoginUser
-}
-
-export const loginPage = () => <LoginUser></LoginUser>
