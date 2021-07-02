991ddc4424637f3ad89941ce7f17a14af5a8e394
minhnh <minhnhde130140@fpt.edu.vn>
2021-07-02 14:53:27 +0700

    fix close pr

--- client/src/components/FormComment/AdviseButton.tsx
+++ client/src/components/FormComment/AdviseButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 //material-ui
-import { Button } from '@material-ui/core';
+import { Button } from '@material-ui/core'
 
 //slyles
 import { useStyles } from './AdviseButton.styles'
@@ -16,34 +16,34 @@ export interface Props {
 /**
  * Primary UI component for user interaction
  */
-export const AdviseButtons: React.FC<Props> = ({
-  instructions
-}) => {
+export const AdviseButtons: React.FC<Props> = ({ instructions }) => {
   const classes = useStyles()
-  const [button, setButton] = React.useState([] as any);
+  const [button, setButton] = React.useState<Array<number>>([])
   const toggleButton = (event: any, index: number) => {
-    console.log(index);
-    console.log(button);
     let array = []
     if (!button.includes(index)) {
       array = [...button, index]
       setButton(array)
-    }
-    else {
-      array = button.filter((x: any) => x != index)
+    } else {
+      array = button.filter((x: number) => x != index)
       setButton(array)
     }
-
   }
   return (
     <>
-      {instructions?.map(
-        (instruction, index) => (
-          <Button id={`${index}`} variant="outlined" onClick={(e: any) => toggleButton(e, index)} color="secondary" className={`${classes.button} ${button.indexOf(index) > -1 ? classes.activeButton : ''}`}>
-            {instruction}
-          </Button>
-        )
-      )}
+      {instructions?.map((instruction, index) => (
+        <Button
+          id={`${index}`}
+          variant="outlined"
+          onClick={(e: any) => toggleButton(e, index)}
+          color="secondary"
+          className={`${classes.button} ${
+            button.indexOf(index) > -1 ? classes.activeButton : ''
+          }`}
+        >
+          {instruction}
+        </Button>
+      ))}
     </>
   )
 }
