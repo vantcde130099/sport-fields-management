b0035bbe4db54e02c052a90ee7378c2164c41e31
minhnh <minhnhde130140@fpt.edu.vn>
2021-07-02 14:22:35 +0700

    init commentComponent

---
+++ client/src/components/Comment/index.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { Story, Meta } from '@storybook/react'
+
+import { Comment, Props } from './index'
+
+export default {
+  title: 'Comment',
+  component: Comment,
+} as Meta
+
+const Template: Story<Props> = (args) => <Comment {...args} />
+
+export const CommentUser = Template.bind({})
+
+CommentUser.args = {
+  img : 'https://www.dungplus.com/wp-content/uploads/2019/12/girl-xinh-1-480x600.jpg',
+  comment : 'Sân xịn đẹp đẽ , Chuyên nghiệp ,Sân xịn đẹp đẽ , Sân xịn đẹp đẽ , Sân xịn đẹp đẽ , ',
+  name : 'Nguyễn Hoàng Minh',
+  date : '11/5/1999',
+  point : 4.5
+}
---
+++ client/src/components/Comment/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+//material-ui
+import { Grid, Paper, Typography, ButtonBase, Avatar } from '@material-ui/core'
+import Rating from '@material-ui/lab/Rating';
+
+//styles
+import { useStyles } from './index.styles'
+
+export interface Props {
+  img?: string
+  date?: string
+  name?: string
+  comment?: string
+  point?: number
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Comment: React.FC<Props> = ({
+  img, name, comment, date, point
+}) => {
+  const classes = useStyles({
+  })
+
+  return (
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item>
+            <Avatar src={img} >{name?.charAt(0)}</Avatar>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Grid container direction='row' justify='space-between' alignItems="center">
+                  <Typography gutterBottom variant="subtitle1">
+                    {name}
+                  </Typography>
+                  <Grid
+                    style={{ color: '#ffb837', width: 'auto', marginBottom: 5.6 }}
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center">
+                    <Typography gutterBottom style={{ margin: 0 }} variant="subtitle1">
+                      {point}
+                    </Typography> <Rating name="size-small" defaultValue={point} size="small" precision={0.5} readOnly />
+                  </Grid>
+                </Grid >
+                <Typography variant="body2" gutterBottom>
+                  {comment}
+                </Typography>
+
+              </Grid>
+              <Grid item>
+                <Typography variant="body2" color="textSecondary">
+                  Ngày : {date}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+  )
+}
